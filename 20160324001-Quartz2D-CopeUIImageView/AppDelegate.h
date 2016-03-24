e679ec3f6760e6e7c01240662adf74f50ea13d39
raineryue <it_jinlei.yue@163.com>
2016-03-24 22:01:09 +0800

    Initial Commit

---
+++ 20160324001-Quartz2D-CopeUIImageView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20160324001-Quartz2D-CopeUIImageView
+//
+//  Created by Rainer on 16/3/24.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 20160324001-Quartz2D-CopeUIImageView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20160324001-Quartz2D-CopeUIImageView
+//
+//  Created by Rainer on 16/3/24.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
